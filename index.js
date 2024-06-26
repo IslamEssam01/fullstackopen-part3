2dff163122053aacc69524924c1a7fe33a3b01c3
IslamEssam01 <essamislam260105@gmail.com>
2024-06-27 02:03:14 +0300

    trying to make frontend work with vercel

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 app.use(cors());
 app.use(
